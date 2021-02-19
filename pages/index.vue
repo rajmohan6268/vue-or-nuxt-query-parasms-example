955b31bdfd9af772f1dd2e2776c0118831ad3c43
rajmohan6268 <48435037+rajmohan6268@users.noreply.github.com>
2021-02-20 01:01:13 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class=" m-8">
+    <div class=" grid grid-cols-3      ">
+      <div v-for="(album ,index) in albums" :key="index" class="p-8 h-64">
+        <n-link class="m-auto" :to="'/photos/'+album.id">
+          <!-- <div>       userId  {{ album.userId }}</div>  https://jsonplaceholder.typicode.com/photos?albumId=6-->
+          <div>     album id     {{ album.id }}</div>
+          <div>     title {{ album.title }}</div>
+        </n-link>
+      </div>
+    </div>
+    <!-- <div class="">
+      <div v-for="photo in photos" :key="photo" class="">
+        {{ photo }}
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      albums: [],
+      photos: []
+    }
+  },
+  mounted () {
+    this.getAlbums()
+  //  this.getPhotos()
+  },
+  methods: {
+    async getAlbums () {
+      const result = await this.$axios.get('https://jsonplaceholder.typicode.com/albums')
+
+      this.albums = result.data
+    }
+    //,
+    // async  getPhotos () {
+    //   const response = await this.$axios.get('https://jsonplaceholder.typicode.com/photos')
+
+    //   this.photos = response.data
+    // }
+  }
+}
+</script>
---
+++ pages/photos/_id.vue
@@ -0,0 +1,46 @@
+<template class="">
+  <div class=" grid grid-cols-3 h-64">
+    <div v-for="(photo,index) in photos " :key="index" class="">
+      <!-- {{ photo}} -->
+      <div class="">
+        albumId  {{ photo.albumId }}
+      </div>
+      <div class="">
+        id {{ photo.id }}
+      </div>
+      <div class="">
+        title  {{ photo.title }}
+      </div>
+      <!-- url  {{ photo.url }} -->
+      <img class="w-40" :src="photo.url">
+      <!-- thumbnailUrl{{ photo.thumbnailUrl }} -->
+      <!-- <img class="w-40" :src="photo.thumbnailUrl"> -->
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      photos: ""
+    };
+  },
+  mounted() {
+    console.log(this.$route.params.id);
+    this.getPhotos();
+  },
+  methods: {
+    async getPhotos() {
+      const response = await this.$axios.get(
+        `https://jsonplaceholder.typicode.com/photos?albumId=${this.$route.params.id}`
+      );
+
+      this.photos = response.data
+    }
+  }
+};
+</script>
